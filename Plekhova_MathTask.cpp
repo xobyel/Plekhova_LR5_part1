1f25a506b6854b44b75d8399cdc362715cba0383
xobyel <plehovaulia3@gmail.com>
2025-04-07 14:58:36 +0300

    change_1 Plekhova_MathTask.cpp

--- Plekhova_MathTask.cpp
+++ Plekhova_MathTask.cpp
@@ -1,19 +1,52 @@
-#include <iostream>
+include <iostream>
+
+
+
 #include "Surname_MathTask.h"
+
+
 
 using namespace std;
 
+
+
 int main()
-{
-    int NumberA = 0, NumberB = 0;
-    // Ввод переменной A
-    EnterDigit(NumberA, "input height A");
-    // Ввод переменной B
-    EnterDigit(NumberB, "input height B");
-    // Вычисление площади прямоугольника
-    int RectangleArea = CalcRectangleArea(NumberA, NumberB);
-    // Вывод значения площади
-    cout << "Area of Rectangle is "<< RectangleArea << endl;
-    
-    return 0;
+
+
+
+}
+
+
+int NumberA = 0, NumberB = 0;
+
+
+
+//ввод переменной А
+
+
+
+EnterDigit (NumberA, "input height A");
+
+
+
+//ввод переменной В
+
+
+
+EnterDigit (NumberB, "input height B");
+
+
+
+//вычисление площади прямоугольника
+
+
+
+int RectangleArea = CalcRectangleArea (NumberA, NumberB);
+
+
+//вывод значения площади
+
+
+cout << "Area of Rectangle is << RectangleArea << endl;
+
 }
