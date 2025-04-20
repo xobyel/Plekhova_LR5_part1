7e098c2aa09739706d8e532d3db631bd220a8a79
xobyel <plehovaulia3@gmail.com>
2025-04-20 14:53:28 +0300

    change

--- Plekhova_MathTask.h
+++ Plekhova_MathTask.h
@@ -3,37 +3,7 @@
 
 #include <string>
 
-using namespace std;
-
-//функция контроля вводимых данных
-bool UserInput(string input) {
-    //если строка пустая - ввод некорректен
-    if (input.empty()) return false;
-    //попытаться
-    try {
-        //преобразование введенного значения в тип
-        int number = stoi(input);
-    }
-    catch (...) //если возникла ошибка в блоке try
-    { return false; }
-    return true;
-}
-
-//метод ввода данных
-void EnterDigit(int& varLink, const string& label) {
-    string raw_input;
-    cout << label << " = ";
-    getline(cin, raw_input);
-    while (!UserInput(raw_input)) {
-        cout << label << " = ";
-        getline(cin, raw_input);
-    }
-    varLink = stoi(raw_input);
-}
-
-//вычисление площади прямоугольника
-int CalRectangleArea(int NumberA, int NumberB) {
-    return NumberA * NumberB;
-}
+int CalcRectangleArea(int NumberA, int NumberB);  // Было Mint вместо int
+void EnterDigit(int& varLink, const std::string& label);
 
 #endif
