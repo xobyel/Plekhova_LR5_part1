959e9281be188be54aae3ca715c35308fbd4c43f
xobyel <plehovaulia3@gmail.com>
2025-04-20 14:53:19 +0300

    change

--- Plekhova_MathTask.cpp
+++ Plekhova_MathTask.cpp
@@ -1,56 +1,14 @@
-#ifndef _PLEKHOVA_MATHTASK_H
-#define _PLEKHOVA_MATHTASK_H
-#include <string> 
+#include "Plekhova_MathTask.h"
+#include <iostream>  // Добавлено для cout/cin
+#include <string>    // Уже было
 
-using namespace std;
-
-//функция контроля вводимых данных 
-bool UserInput (string input) {
-
-//если строка пустая ввод некорректен 
-if (input.empty()) return false;
-
-//попытаться
-
-try {
-//преобразование введенного значения в тип 
-int number = stoi(input);
+void EnterDigit(int& varLink, const std::string& label) {
+    std::cout << label << " = ";  // Исправлено <<< на <<
+    std::string raw_input;
+    std::getline(std::cin, raw_input);  // Добавлен std::
+    varLink = std::stoi(raw_input);     // Исправлена строка с stoi
 }
 
-catch (...) // если возникла ошибка в блоке try
-
-{return false; }
-
-return true;
-
+int CalcRectangleArea(int NumberA, int NumberB) {  // Было Mint вместо int
+    return NumberA * NumberB;
 }
-
-//метод ввода данных
-
-void EnterDigit(int& varLink, const string& label) {
-
-string raw_input;
-
-cout <<< label << " = ";
-
-getline(cin, raw_input);
-
-while (!UserInput (raw_input)) {
-
-cout << label << " = ";
-
-getline(cin, raw_input);
-}
-
-varLink stoi(raw_input);
-
-}
-
-//вычисление площади прямоугольника
-
-Mint CalcRectangleArea (int NumberA, int NumberB) {
-
-return NumberA * NumberB;
-}
-
-#endif
