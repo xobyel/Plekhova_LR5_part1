ec0f2b744bba3a9fe594865e07f0cb49a17e28b9
xobyel <plehovaulia3@gmail.com>
2025-04-21 09:14:50 +0300

    change tests

--- Plekhova_Test_MathTask.cpp
+++ Plekhova_Test_MathTask.cpp
@@ -1,33 +1,48 @@
+#include "Plekhova_MathTask.h"
 #include "CUTE/cute/cute.h"
 #include "CUTE/cute/cute_runner.h"
 #include "CUTE/cute/ide_listener.h"
-#include "Plekhova_MathTask.h"
 
-// Базовые тест-кейсы
-void test_normal_case() {
-    ASSERT_EQUAL(15, CalcRectangleArea(3, 5));
+// Тест на пустые значения
+void testUserInput_Empty() {
+    std::string str = "";
+    bool expected = false;
+    bool actual = UserInput(str);
+    ASSERT_EQUAL(expected, actual);
 }
 
-void test_zero_width() {
-    ASSERT_EQUAL(0, CalcRectangleArea(0, 10));
+// Тест на нецифровые символы
+void testUserInput_Letter() {
+    std::string str = "a";
+    bool expected = false;
+    bool actual = UserInput(str);
+    ASSERT_EQUAL(expected, actual);
 }
 
-void test_zero_height() {
-    ASSERT_EQUAL(0, CalcRectangleArea(7, 0));
+// Тест на цифро-буквенные значения
+void testUserInput_DigitLetterValue() {
+    std::string str = "5a";
+    bool expected = false;
+    bool actual = UserInput(str);
+    ASSERT_EQUAL(expected, actual);
 }
 
-void test_negative_values() {
-    ASSERT_EQUAL(0, CalcRectangleArea(-5, 4));
+// Тест на отрицательные значения
+void testUserInput_NegativeValue() {
+    std::string str = "-5";
+    bool expected = false;
+    bool actual = UserInput(str);
+    ASSERT_EQUAL(expected, actual);
 }
 
 int main() {
     cute::suite s;
-    s.push_back(CUTE(test_normal_case));
-    s.push_back(CUTE(test_zero_width));
-    s.push_back(CUTE(test_zero_height));
-    s.push_back(CUTE(test_negative_values));
+    s.push_back(CUTE(testUserInput_Empty));
+    s.push_back(CUTE(testUserInput_Letter));
+    s.push_back(CUTE(testUserInput_DigitLetterValue));
+    s.push_back(CUTE(testUserInput_NegativeValue));
     
     cute::ide_listener<> listener;
-    cute::makeRunner(listener)(s, "Rectangle Area Tests");
+    cute::makeRunner(listener)(s, "User Input Validation Tests");
     return 0;
 }
