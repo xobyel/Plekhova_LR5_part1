7eeeca147afeab248ada434ae3aad2c2baa1eed4
xobyel <plehovaulia3@gmail.com>
2025-04-21 09:31:37 +0300

    change mathtask

--- Plekhova_MathTask.cpp
+++ Plekhova_MathTask.cpp
@@ -1,7 +1,18 @@
 #include "Plekhova_MathTask.h"
+#include <cctype>
 
-int CalcRectangleArea(int a, int b) {
-    // Добавляем проверку на отрицательные значения
-    if (a <= 0 || b <= 0) return 0;
-    return a * b;
+bool UserInput(const std::string& input) {
+    // Проверка на пустую строку
+    if (input.empty()) return false;
+    
+    // Проверка на отрицательные числа
+    if (input[0] == '-') return false;
+    
+    // Проверка что все символы - цифры
+    for (char c : input) {
+        if (!isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
 }
