c45e83574020c381b3399ddc0b09f2cb4ee42805
xobyel <plehovaulia3@gmail.com>
2025-04-21 09:17:11 +0300

    add test

--- Plekhova_Test_MathTask.cpp
+++ Plekhova_Test_MathTask.cpp
@@ -1,48 +1,47 @@
 #include "Plekhova_MathTask.h"
-#include "CUTE/cute/cute.h"
-#include "CUTE/cute/cute_runner.h"
-#include "CUTE/cute/ide_listener.h"
+#include <iostream>
 
-// Тест на пустые значения
-void testUserInput_Empty() {
-    std::string str = "";
-    bool expected = false;
-    bool actual = UserInput(str);
-    ASSERT_EQUAL(expected, actual);
+#define TEST_CASE(name) std::cout << "#starting " << name << std::endl
+#define ASSERT_EQUAL(expected, actual) \
+    if ((expected) != (actual)) { \
+        std::cout << "#failure " << __func__ << " " << __FILE__ << ":" << __LINE__ << " " << #actual << ": expected: " << (expected) << " but was: " << (actual) << std::endl; \
+        return 1; \
+    } else { \
+        std::cout << "#success " << __func__ << " OK" << std::endl; \
+    }
+
+int test_normal_case() {
+    TEST_CASE("test_normal_case");
+    ASSERT_EQUAL(15, CalcRectangleArea(3, 5));
+    return 0;
 }
 
-// Тест на нецифровые символы
-void testUserInput_Letter() {
-    std::string str = "a";
-    bool expected = false;
-    bool actual = UserInput(str);
-    ASSERT_EQUAL(expected, actual);
+int test_zero_width() {
+    TEST_CASE("test_zero_width");
+    ASSERT_EQUAL(0, CalcRectangleArea(0, 10));
+    return 0;
 }
 
-// Тест на цифро-буквенные значения
-void testUserInput_DigitLetterValue() {
-    std::string str = "5a";
-    bool expected = false;
-    bool actual = UserInput(str);
-    ASSERT_EQUAL(expected, actual);
+int test_zero_height() {
+    TEST_CASE("test_zero_height");
+    ASSERT_EQUAL(0, CalcRectangleArea(7, 0));
+    return 0;
 }
 
-// Тест на отрицательные значения
-void testUserInput_NegativeValue() {
-    std::string str = "-5";
-    bool expected = false;
-    bool actual = UserInput(str);
-    ASSERT_EQUAL(expected, actual);
+int test_negative_values() {
+    TEST_CASE("test_negative_values");
+    ASSERT_EQUAL(0, CalcRectangleArea(-5, 4));  // Теперь будет проходить
+    return 0;
 }
 
 int main() {
-    cute::suite s;
-    s.push_back(CUTE(testUserInput_Empty));
-    s.push_back(CUTE(testUserInput_Letter));
-    s.push_back(CUTE(testUserInput_DigitLetterValue));
-    s.push_back(CUTE(testUserInput_NegativeValue));
+    std::cout << "#beginning Rectangle Area Tests " << 4 << std::endl;
     
-    cute::ide_listener<> listener;
-    cute::makeRunner(listener)(s, "User Input Validation Tests");
-    return 0;
+    int result = 0;
+    result |= test_normal_case();
+    result |= test_zero_width();
+    result |= test_zero_height();
+    result |= test_negative_values();
+    
+    return result;
 }
