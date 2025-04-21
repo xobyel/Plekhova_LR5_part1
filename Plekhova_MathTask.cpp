6bf90127cdf9a903a30158600aff97679d7a093e
xobyel <plehovaulia3@gmail.com>
2025-04-21 09:17:35 +0300

    add mathtask

--- Plekhova_MathTask.cpp
+++ Plekhova_MathTask.cpp
@@ -1,14 +1,7 @@
 #include "Plekhova_MathTask.h"
-#include <iostream>  // Добавлено для cout/cin
-#include <string>    // Уже было
 
-void EnterDigit(int& varLink, const std::string& label) {
-    std::cout << label << " = ";  // Исправлено <<< на <<
-    std::string raw_input;
-    std::getline(std::cin, raw_input);  // Добавлен std::
-    varLink = std::stoi(raw_input);     // Исправлена строка с stoi
-}
-
-int CalcRectangleArea(int NumberA, int NumberB) {  // Было Mint вместо int
-    return NumberA * NumberB;
+int CalcRectangleArea(int a, int b) {
+    // Добавляем проверку на отрицательные значения
+    if (a <= 0 || b <= 0) return 0;
+    return a * b;
 }
