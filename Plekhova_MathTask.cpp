eb3026f790b3d3c0d982e26f95dd6d28352cf192
xobyel <plehovaulia3@gmail.com>
2025-04-21 10:52:03 +0300

    change

--- Plekhova_MathTask.cpp
+++ Plekhova_MathTask.cpp
@@ -1,6 +1,13 @@
 #include "Plekhova_MathTask.h"
 #include <cctype>
 
+#include "Plekhova_MathTask.h"
+
+// Реализация функции
+int CalcRectangleArea(int a, int b) {
+    return a * b;
+}
+
 bool UserInput(const std::string& input) {
     // Проверка на пустую строку
     if (input.empty()) return false;
