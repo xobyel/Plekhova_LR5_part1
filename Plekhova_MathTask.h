0f11244cff1017673b86aed8d726c7c68e5b7cfa
xobyel <plehovaulia3@gmail.com>
2025-04-21 10:51:55 +0300

    change

--- Plekhova_MathTask.h
+++ Plekhova_MathTask.h
@@ -1,9 +1,7 @@
 #ifndef PLEKHOVA_MATHTASK_H
 #define PLEKHOVA_MATHTASK_H
 
-#include <string>
-
-// Функция проверки ввода пользователя
-bool UserInput(const std::string& input);
+// Объявление функции
+int CalcRectangleArea(int a, int b);
 
 #endif
